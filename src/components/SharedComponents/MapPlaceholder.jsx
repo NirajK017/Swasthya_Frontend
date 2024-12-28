9920eb16ef20bf03d25d7621468f3bede040fc7c
Atharva6780 <atharvas6780@gmail.com>
2024-12-28 15:38:10 +0530

    Add Geoapify integration for nearby hospitals and update MapPlaceholder component

--- src/components/SharedComponents/MapPlaceholder.jsx
+++ src/components/SharedComponents/MapPlaceholder.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
-const MapPlaceholder = () => {
+const MapPlaceholder = ({ userLocation, hospitals }) => {
+  // Fix for Leaflet default icon issue (if icons don't show)
+  useEffect(() => {
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+      shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    });
+  }, []);
+
   return (
-    <div className="w-full h-[400px] bg-gray-200 flex items-center justify-center text-gray-500">
-      Map Placeholder
+    <div className="w-full h-[400px] bg-gray-200">
+      {userLocation && (
+        <MapContainer
+          center={userLocation}
+          zoom={13}
+          style={{ height: "100%", width: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={userLocation}>
+            <Popup>Your Location</Popup>
+          </Marker>
+          {hospitals.map((hospital, index) => (
+            <Marker key={index} position={hospital.location}>
+              <Popup>
+                <b>{hospital.name}</b>
+                <br />
+                {hospital.address}
+              </Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      )}
     </div>
   );
 };
