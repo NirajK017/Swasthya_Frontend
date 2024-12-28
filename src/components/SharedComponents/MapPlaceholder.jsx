1c74d366c31cc0be00750891d784132109027317
Mohitpatil344 <mr.mohitpatil003@gmail.com>
2024-12-28 16:01:28 +0530

    changes

--- src/components/SharedComponents/MapPlaceholder.jsx
+++ src/components/SharedComponents/MapPlaceholder.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const MapPlaceholder = () => {
+const   MapPlaceholder = () => {
   return (
     <div className="w-full h-[400px] bg-gray-200 flex items-center justify-center text-gray-500">
       Map Placeholder
